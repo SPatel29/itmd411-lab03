ba2270359a153c0f05ea21ba9fd7bcd8f660cd54
SPatel29 <sunnypatelc9@gmail.com>
2022-03-13 18:55:25 -0500

    added comments describing code

--- src/Records.java
+++ src/Records.java
@@ -13,7 +13,6 @@ public class Records extends BankRecords {
             System.out.println("File was not found. Check typos");
         } catch (Exception ex) {
             ex.printStackTrace();
-            System.out.println("Soemthign went wrong when trying to read the file");
         }
     }
 
@@ -21,11 +20,11 @@ public class Records extends BankRecords {
         Records br = new Records();
         br.read_data();
         br.process_data();
-        AvgComp();
-        females_savings_account();
-        male_car_child();
-        avg_income_over_50();
-        location_counter();
+        AvgComp(); // counts avergae income of female and male employees
+        females_savings_account(); // counts females that have a savings account
+        male_car_child(); // counts males that have car, 1 kid and per region
+        avg_income_over_50(); // counts average income over 50
+        location_counter(); // counts location of all employees
         try {
             fw.close();
         } catch (Exception e) {
@@ -33,6 +32,12 @@ public class Records extends BankRecords {
         }
     }
 
+    /*
+     * gets average income of all employees
+     * diffentiating from sex using its sex comparator
+     * This method takes no arguments and returns no value
+     * 
+     */
     private static void AvgComp() {
         Arrays.sort(records, new SexComparator());
         int male_count = 0, female_count = 0;
@@ -64,11 +69,17 @@ public class Records extends BankRecords {
         }
     }
 
+    /*
+     * Counts females that have savings account and a mortgage
+     * This method takes no arguments and does not return a value
+     */
     private static void females_savings_account() {
         int num_of_females = 0;
-        Arrays.sort(records, new MortgageComparator());
-        Arrays.sort(records, new SavingsAccountComparator());
-        Arrays.sort(records, new SexComparator());
+        // The below sorts the array by sex first, then sorts by savings account,
+        // then sorts by mortgage
+        Arrays.sort(records,
+                new SexComparator().thenComparing(new SavingsAccountComparator())
+                        .thenComparing(new MortgageComparator()));
         for (int person = 0; person < records.length; person += 1) {
             if (records[person].get_sex().equals("FEMALE") && records[person].get_save_act().equals("YES")
                     && records[person].get_mortgage().equals("YES")) {
@@ -88,7 +99,14 @@ public class Records extends BankRecords {
         }
     }
 
+    /*
+     * The below method counts males that have a car, have 1 child only and
+     * seperates them by regions
+     * This method takes no arguments and returns nothing
+     */
     private static void male_car_child() {
+        // The below first sorts by sex, then by number of children
+        // then by location, then by if they have a car or not
         Arrays.sort(records, new SexComparator().thenComparing(new ChildComparator())
                 .thenComparing(new LocationComparator()).thenComparing(new CarComparator()));
 
@@ -112,25 +130,25 @@ public class Records extends BankRecords {
                 male_count_suburban += 1;
             }
         }
-        System.out.printf("\nNumber of males with a car and 1 child only in inner city region: %d\n",
+        System.out.printf("\nNumber of males with a car and 1 child only in INNER CITY region: %d\n",
                 male_count_inner_city);
-        System.out.printf("\nNumber of males with a car and 1 child only in town region: %d\n",
+        System.out.printf("\nNumber of males with a car and 1 child only in TOWN region: %d\n",
                 male_count_town);
-        System.out.printf("\nNumber of males with a car and 1 child only in rural region: %d\n",
+        System.out.printf("\nNumber of males with a car and 1 child only in RURAL region: %d\n",
                 male_count_rural);
-        System.out.printf("\nNumber of males with a car and 1 child only in suburban region: %d\n",
+        System.out.printf("\nNumber of males with a car and 1 child only in SUBURBAN region: %d\n",
                 male_count_suburban);
 
         try {
             fw.write(
-                    "Number of males with a car and 1 child only in inner city region: " + male_count_inner_city
+                    "Number of males with a car and 1 child only in INNER CITY region: " + male_count_inner_city
                             + '\n');
             fw.write(
-                    "Number of males with a car and 1 child only in town region: " + male_count_town + '\n');
+                    "Number of males with a car and 1 child only in TOWN region: " + male_count_town + '\n');
             fw.write(
-                    "Number of males with a car and 1 child only in location region: " + male_count_rural + '\n');
+                    "Number of males with a car and 1 child only in LOCATION region: " + male_count_rural + '\n');
             fw.write(
-                    "Number of males with a car and 1 child only in suburban region: " + male_count_suburban + '\n');
+                    "Number of males with a car and 1 child only in SUBURBAN region: " + male_count_suburban + '\n');
         } catch (FileNotFoundException ex) {
             System.out.println("File not found. Check Typos");
         } catch (IOException ex) {
@@ -140,7 +158,11 @@ public class Records extends BankRecords {
         }
     }
 
+    /*
+     * The below gets the average income of people over the age of 50
+     */
     private static void avg_income_over_50() {
+        // The below sorts by age
         Arrays.sort(records, new AgeComparator());
         double income = 0;
         int quantity = 0;
@@ -164,25 +186,28 @@ public class Records extends BankRecords {
         }
     }
 
+    // The method below counts how many employees in each locatino
+    // This method takes no arguments and returns no value
     private static void location_counter() {
-        Arrays.sort(records, new AgeComparator());
+        // The below sorts by location
+        Arrays.sort(records, new LocationComparator());
         int inner_city_count = 0;
         int town_count = 0;
         int rural_count = 0;
         int suburban_count = 0;
 
-        for (int person = 0; person < records.length; person += 1){
+        for (int person = 0; person < records.length; person += 1) {
             if (records[person].get_region().equals("INNER_CITY"))
                 inner_city_count += 1;
-            
-            else if (records[person].get_region().equals("TOWN")) 
+
+            else if (records[person].get_region().equals("TOWN"))
                 town_count += 1;
-            
-            else if (records[person].get_region().equals("RURAL")) 
+
+            else if (records[person].get_region().equals("RURAL"))
                 rural_count += 1;
-            
-            else if (records[person].get_region().equals("SUBURBAN")) 
-                suburban_count += 1;   
+
+            else if (records[person].get_region().equals("SUBURBAN"))
+                suburban_count += 1;
         }
         System.out.printf("\nNumber of employees in INNER CITY region: %d\n", inner_city_count);
         System.out.printf("\nNumber of employees in TOWN region: %d\n", town_count);
