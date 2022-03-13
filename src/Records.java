3e8677379b32fd97544b9a435de1fd8d23fe53a6
SPatel29 <sunnypatelc9@gmail.com>
2022-03-13 15:37:14 -0500

    Added Age Comparator and implemented avg income over 50 and region count methods

--- src/Records.java
+++ src/Records.java
@@ -24,6 +24,8 @@ public class Records extends BankRecords {
         AvgComp();
         females_savings_account();
         male_car_child();
+        avg_income_over_50();
+        location_counter();
         try {
             fw.close();
         } catch (Exception e) {
@@ -87,10 +89,9 @@ public class Records extends BankRecords {
     }
 
     private static void male_car_child() {
-        Arrays.sort(records, new CarComparator());
-        Arrays.sort(records, new ChildComparator());
-        Arrays.sort(records, new LocationComparator());
-        Arrays.sort(records, new SexComparator());
+        Arrays.sort(records, new SexComparator().thenComparing(new ChildComparator())
+                .thenComparing(new LocationComparator()).thenComparing(new CarComparator()));
+
         int male_count_inner_city = 0;
         int male_count_town = 0;
         int male_count_rural = 0;
@@ -122,13 +123,78 @@ public class Records extends BankRecords {
 
         try {
             fw.write(
-                    "Number of males with a car and 1 child only in inner city region: " + male_count_inner_city + '\n');
+                    "Number of males with a car and 1 child only in inner city region: " + male_count_inner_city
+                            + '\n');
             fw.write(
                     "Number of males with a car and 1 child only in town region: " + male_count_town + '\n');
             fw.write(
                     "Number of males with a car and 1 child only in location region: " + male_count_rural + '\n');
             fw.write(
                     "Number of males with a car and 1 child only in suburban region: " + male_count_suburban + '\n');
+        } catch (FileNotFoundException ex) {
+            System.out.println("File not found. Check Typos");
+        } catch (IOException ex) {
+            System.out.println("IO Error");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private static void avg_income_over_50() {
+        Arrays.sort(records, new AgeComparator());
+        double income = 0;
+        int quantity = 0;
+        for (int person = 0; person < records.length; person += 1) {
+            if (records[person].get_age() >= 50) {
+                income += records[person].get_income();
+                quantity += 1;
+            }
+        }
+        System.out.printf("\nAverage income for employees greater than or equal to the age 50 is: $%.2f\n",
+                (income / quantity));
+        try {
+            fw.write("Average income for employees greater than or equal to the age 50 is: $"
+                    + String.format("%.2f", (income / quantity)) + '\n');
+        } catch (FileNotFoundException ex) {
+            System.out.println("File not found. Check Typos");
+        } catch (IOException ex) {
+            System.out.println("IO Error");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private static void location_counter() {
+        Arrays.sort(records, new AgeComparator());
+        int inner_city_count = 0;
+        int town_count = 0;
+        int rural_count = 0;
+        int suburban_count = 0;
+
+        for (int person = 0; person < records.length; person += 1){
+            if (records[person].get_region().equals("INNER_CITY"))
+                inner_city_count += 1;
+            
+            else if (records[person].get_region().equals("TOWN")) 
+                town_count += 1;
+            
+            else if (records[person].get_region().equals("RURAL")) 
+                rural_count += 1;
+            
+            else if (records[person].get_region().equals("SUBURBAN")) 
+                suburban_count += 1;   
+        }
+        System.out.printf("\nNumber of employees in INNER CITY region: %d\n", inner_city_count);
+        System.out.printf("\nNumber of employees in TOWN region: %d\n", town_count);
+        System.out.printf("\nNumber of employees in RURAL region: %d\n", rural_count);
+        System.out.printf("\nNumber of employees in SUBURBAN region: %d\n", suburban_count);
+
+        try {
+            fw.write("Number of employees in INNER CITY region: " + String.format("%d", inner_city_count) + '\n');
+            fw.write("Number of employees in TOWN region: " + String.format("%d", town_count) + '\n');
+            fw.write("Number of employees in RURAL region: " + String.format("%d", rural_count) + '\n');
+            fw.write("Number of employees in SUBURBAN region: " + String.format("%d", suburban_count) + '\n');
+
         } catch (FileNotFoundException ex) {
             System.out.println("File not found. Check Typos");
         } catch (IOException ex) {
